fbead304d7823ffa87d0a7daa9348a5b5521ce99
xoin <xoin@xoin.nl>
2017-12-20 12:50:39 +0100

    Rename script

---
+++ Powershell/Renamebyfilelength.ps1
@@ -0,0 +1,8 @@
+﻿#Bulk renamer of files when I screw up.
+
+$files = @{
+"569146927" = "1 D1.mkv"; 
+"361297402" = "1_D2.mkv";
+}
+
+Get-ChildItem -Filter "*" -Recurse | Rename-Item -NewName {$files.Get_Item($_.Length.toString()) }
